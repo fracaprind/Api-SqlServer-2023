c2cfdcbd3a9cc6f32d4f4062fc4d2bf0b0f9db49
caprind <caprind@caprind.com.br>
2023-05-31 18:52:15 -0300

    Correção nova rota

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -24,7 +24,7 @@ export const postLogin = async (req, res) => {
 
     if (senha !== user.Senha) {
         console.log(senha)
-        console.log(user)
+        console.log(user.senha)
 
         return res.status(400).json({
             erro: true,

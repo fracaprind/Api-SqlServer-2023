6bc50aa9d008ea68e2670fa4730175e0305ebae8
caprind <caprind@caprind.com.br>
2023-05-31 19:39:16 -0300

    Correção nova no login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -8,13 +8,13 @@ export const postLogin = async (req, res) => {
     const senha = req.body.senha
     const email = req.body.email
 
-    console.log('Requisição : ',req.body)
+ //   console.log('Requisição : ',req.body)
 
     const result = await pool.request()
     .input("Email",email)
     .query(querys.getLogin)
 
-   const user = result.recordset
+   const user = result.recordset[0]
     if (user === null) {
         return res.status(400).json({
             erro: true,
@@ -22,22 +22,29 @@ export const postLogin = async (req, res) => {
         });
     };
 
-    if (senha !== user[0].Senha) {
-        console.log(senha)
-        console.log(user.senha)
+    console.log('Senha recebida: ',senha)
+    console.log('Senha banco', user.Senha)
 
+    if (senha !== user.Senha) {
         return res.status(400).json({
             erro: true,
             mensagem: "Erro: Usuário ou a senha incorreta!"
         });
     };
 
-    console.log('Senha: ',senha)
-    console.log('Dados usuario retornado', user)
+    if (senha === user.Senha) {
+ //   console.log('Senha: ',senha)
+ //   console.log('Dados usuario retornado', user)
 
     return res.json({
         erro: false,
         mensagem: "Login realizado com sucesso!",
-        user: { Nome: user.Nome }
+        user: { Email: user.Email }
     });
+    }else{
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Usuário ou a senha incorreta!"
+        });
+    }
 }
